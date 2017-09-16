a9a7fcf2acc91ca47a0054661444c65b6d831f7c
waterlzj <waterlzj@gmail.com>
2017-09-17 03:46:09 +0800

    g13, 日志分析脚本优化

--- script/info.sh
+++ script/info.sh
@@ -9,4 +9,4 @@ grep '创建房间成功' $path/log/lobby-1.log.$date-* |wc -l
 echo "总结算次数:"
 grep '总结算完成' $path/log/lobby-1.log.$date-* |wc -l
 echo "当前连接数"
-netstat -natp|grep gateway_exec|grep ESTABLISHED|grep -v '127.0.0.1:6000' |wc -l
+netstat -natp|grep gateway_exec|grep ESTABLISHED|grep -v '127.0.0.1:6000' |grep -v ':8001' | wc -l
