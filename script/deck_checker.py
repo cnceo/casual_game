c91d2910346ec9fe508e5d401305a1ac692341c1
waterlzj <waterlzj@gmail.com>
2017-09-20 08:21:08 +0800

    牌型日志过滤脚本， 完成

--- script/deck_checker.py
+++ script/deck_checker.py
@@ -24,21 +24,49 @@ class LogFile(object):
 
 class Logs(object):
     def __init__(self, directory):
-        self.files = {}
+        self.files = []
         for filename in os.listdir(directory):
             path = os.path.join(directory, filename)
             if os.path.isfile(path):
-                self.files[path] = LogFile(path)
+                self.files.append(LogFile(path))
         pass
 
     def grepQingLong(self):
-        pass
+        for f in self.files:
+            for l in f.lines:
+                if l.find('deal deck') == -1:
+                    continue
+                ret = re.search('cards=\[.+\]', l)
+                if not ret:
+                    continue
+                begin, end = ret.span()
+                cardstrs = l[begin + 7, end - 2].split(',')
+                #print(cardstrs)
+                ranks = [(int(cardstr) - 1) % 13 for cardstr in cardstrs]
+                suits = [(int(cardstr) - 1) // 13 for cardstr in cardstrs]
+
+                isStright = cards == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+                isFlush   = len(set(suits)) == 1
+
+                if isStright and isFlush:
+                    print("发牌, 同花大顺")
+                    print(l)
+                elif isStright:
+                    print("发牌, 大顺")
+                    print(l)
+                elif isFlush:
+                    print("发牌, 同花")
+                    print(l)
+
+
+
+
+
 
 
 
 logs = Logs("../log")
-for filename in logs.files:
-    print(filename)
+logs.grepQingLong()
 
 
 
