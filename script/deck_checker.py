5c01d8af3475f247c4de546973d7fb1c3870decb
waterlzj <waterlzj@gmail.com>
2017-09-21 23:41:28 +0800

    g13, 基本的牌形统计和消费统计脚本

--- script/deck_checker.py
+++ script/deck_checker.py
@@ -12,61 +12,60 @@ class LogFile(object):
         self.name = path
         self.lines = []
         f = open(path)
-        for line in f:
-            self.lines.append(line)
+        for l in f:
+            if l.find('deal deck') == -1: 
+                continue
+            self.lines.append(l)
+            #print(l)
 
-    def grepLines(self, *keywords):
-        pass
+    def grepLong(self):
+        for l in self.lines:
+            ret = re.search('cards=\[.+\]', l)
+            if not ret:
+                continue
+            begin, end = ret.span()
+            cardstrs = l[begin + 7: end - 2].split(',')
+            #print(cardstrs)
+            ranks = [(int(cardstr) - 1) % 13 for cardstr in cardstrs]
+            suits = [(int(cardstr) - 1) // 13 for cardstr in cardstrs]
 
-    def egrepLines(self, regular):
-        pass
+            isStright = ranks == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
+            isFlush   = len(set(suits)) == 1
+
+            if isStright:
+                if isFlush:
+                    print("发牌, 青龙")
+                else:
+                    print("发牌, 一条龙")
+                print(l)
+
 
 
 class Logs(object):
     def __init__(self, directory):
-        self.files = []
-        for filename in os.listdir(directory):
-            path = os.path.join(directory, filename)
+        self.directory = directory
+
+
+    def grepLong(self):
+        for filename in os.listdir(self.directory):
+            path = os.path.join(self.directory, filename)
             if os.path.isfile(path):
-                self.files.append(LogFile(path))
-        pass
-
-    def grepQingLong(self):
-        for f in self.files:
-            for l in f.lines:
-                if l.find('deal deck') == -1:
+                if path.find('lobby') == -1: 
                     continue
-                ret = re.search('cards=\[.+\]', l)
-                if not ret:
-                    continue
-                begin, end = ret.span()
-                cardstrs = l[begin + 7, end - 2].split(',')
-                #print(cardstrs)
-                ranks = [(int(cardstr) - 1) % 13 for cardstr in cardstrs]
-                suits = [(int(cardstr) - 1) // 13 for cardstr in cardstrs]
-
-                isStright = cards == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-                isFlush   = len(set(suits)) == 1
-
-                if isStright and isFlush:
-                    print("发牌, 同花大顺")
-                    print(l)
-                elif isStright:
-                    print("发牌, 大顺")
-                    print(l)
-                elif isFlush:
-                    print("发牌, 同花")
-                    print(l)
+                print(path)
+                f = LogFile(path)
+                f.grepLong()
 
 
 
 
+def test():
+    LogFile("./script/lobby-1.log")
 
 
+#test()
 
 
-logs = Logs("../log")
-logs.grepQingLong()
-
-
+logs = Logs("./log")
+logs.grepLong()
 
---
+++ script/money.py
@@ -0,0 +1,72 @@
+#-*-coding:utf-8-*-
+#!/usr/bin/python
+
+import os
+import re
+import sys
+
+
+class LogFile(object):
+    def __init__(self, path):
+        self.name = path
+        self.lines = []
+        self.money = 0
+        f = open(path)
+        for l in f:
+            if l.find('moneyChange') == -1: 
+                continue
+            #print(l)
+            self.lines.append(l)
+
+    def grepMoney(self):
+        for l in self.lines:
+            ret = re.search('moneyChange=-\d+', l)
+            if not ret:
+                continue
+            print(l)
+            begin, end = ret.span()
+            moneystr = l[begin + 12: end]
+            money = int(moneystr)
+            self.money += money
+
+
+
+class Logs(object):
+    def __init__(self, directory):
+        self.directory = directory
+        self.money = 0
+
+
+    def grepMoney(self):
+        for filename in os.listdir(self.directory):
+            path = os.path.join(self.directory, filename)
+            if os.path.isfile(path):
+                if path.find('lobby') == -1 or path.find(sys.argv[1]) == -1: 
+                    continue
+#                print(path)
+                f = LogFile(path)
+                f.grepMoney()
+                self.money += f.money
+        print("date: {}, total: {}".format(sys.argv[1], self.money))
+
+
+
+
+
+def test():
+    LogFile("./script/lobby-1.log")
+
+
+#test()
+
+
+def main():
+    if len(sys.argv) < 2:
+        print "missing date parameter"
+        return
+    logs = Logs("./log")
+    logs.grepMoney()
+
+
+main()
+
